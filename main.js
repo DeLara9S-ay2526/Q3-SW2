6e363bb88293e6413f57520e64152a982b0a276f
DeLara9S-ay2526 <delara.xq@obmontessori.edu.ph>
2026-03-03 07:55:50 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 function cDiscount(){
     let budget = document.getElementById("budget").value;
-    let disVal = budget * .2;
-    document.getElementById("brands-discounts").innerHTML = '<div class="container col-md-4 bg-light text-dark rounded mt-3 text-center" id="brands-discounts"> <h3>Recommended Brands:</h3> <button class="btn btn-outline-primary" type="button">Acer</button> <button class="btn btn-outline-primary" type="button">Lenovo</button> <button class="btn btn-outline-primary" type="button">Dell</button> <h3>Eligible discount: <span class="badge bg-primary" id></span></h3> </div>';
+    let disVal = Number(budget * .2).toFixed(2);
+    document.getElementById("brands-discounts").innerHTML = 
+    `<h3>Recommended Brands:</h3>
+        <button class="btn btn-outline-primary" type="button">Acer</button>
+        <button class="btn btn-outline-primary" type="button">Lenovo</button>
+        <button class="btn btn-outline-primary" type="button">Dell</button>
+    <h3>Eligible discount: <br> <span class="badge bg-primary" id="dis">${disVal}</span></h3>`;
 }
 function cStore(){
-
+    document.getElementById("locations").innerHTML = 
+    `<h2>VillMan</h2>
+     <p>Level 1 (infront of Ramen 99), Trinoma Mall, EDSA corner North Ave., Quezon City</p>
+     <p>5/F Cyberzone SM City North Edsa The Annex, Quezon City</p>`
 }
